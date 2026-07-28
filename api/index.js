5c18204d748e04d6bb41867f56fe1a0f9768605c
Your GitHub Monishkumar55 <monishkumar3200@gmail.com>
2026-07-29 00:02:02 +0530

    Update Vercel rewrites configuration to modern schema

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-const app = require('../songstr/server.js');
+const path = require('path');
+const app = require(path.join(__dirname, '..', 'songstr', 'server.js'));
 
 module.exports = (req, res) => {
   return app(req, res);
